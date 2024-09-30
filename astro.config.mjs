c90cabd925fd01313058c0485f6ea7bd9e6a07cb
Dominic Cassidy <dominic@koroutine.tech>
2024-09-30 16:15:09 +0300

    feat: strip back
    Set placeholder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,39 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://conventionaldevs.org",
+  integrations: [
+    starlight({
+      title: "Conventional Devs",
+      social: {
+        github: "https://github.com/koroutine/conventionaldevs.org",
+      },
+      sidebar: [
+        // {
+        //   label: "Guides",
+        //   items: [
+        //     // Each item here is one entry in the navigation menu.
+        //     { label: "Example Guide", slug: "guides/example" },
+        //   ],
+        // },
+        // {
+        //   label: "Reference",
+        //   autogenerate: { directory: "reference" },
+        // },
+      ],
       customCss: [
         // Path to your Tailwind base styles:
         "./src/tailwind.css",
       ],
+    }),
     tailwind({
       // Disable the default base styles:
       applyBaseStyles: false,
     }),
+  ],
 });
